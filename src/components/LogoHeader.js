b8fcbcea1260886cffc2df360b9e3f98bbbf49ce
amila1998 <amiladevin@gmail.com>
2023-07-24 23:27:26 +0530

    auth logic impl

--- src/components/LogoHeader.js
+++ src/components/LogoHeader.js
@@ -3,15 +3,46 @@ import React from "react";
 import themes from "../common/theme/themes";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
-
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { access, info, logout } from "../actions/authActions";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 const LogoHeader = () => {
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+  const token = useSelector((state) => state.auth.token);
+  const user = useSelector((state) => state.auth.user);
+  const dispatch = useDispatch();
   const navigate = useNavigation();
+  let timeout;
+
+  const fetchData = async () => {
+    const isLocalLogin = await AsyncStorage.getItem("isLogin");
+    if (isLocalLogin && isLoggedIn) {
+      await dispatch(access(navigate));
+    }
+    timeout = setTimeout(fetchData, 2*60*1000);
+  };
+
+  const fetchData2 = async () => {
+    if (token && !user) {
+      await dispatch(info(token, navigate));
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [isLoggedIn, dispatch]);
+
+  useEffect(() => {
+    fetchData2();
+  }, [token, user, dispatch]);
+
   return (
     <View style={styles.container}>
       <Text style={styles.logoText}>AYURMINDS</Text>
       <MaterialCommunityIcons
         size={28}
-        name='chat'
+        name="chat"
         color={themes.Colors.primary}
         onPress={() => navigate.navigate("DocChat")}
       />
--- src/screens/DoctorChat.js
+++ src/screens/DoctorChat.js
@@ -2,14 +2,24 @@ import { Text, StyleSheet, View } from "react-native";
 import React, { Component } from "react";
 import AppNavigator from "../navigations/AppNavigator";
 
-export default class DoctorChat extends Component {
-  render() {
-    return (
-      <View>
-        <Text>DoctorChat</Text>
-      </View>
-    );
-  }
+// export default class DoctorChat extends Component {
+//   render() {
+//     return (
+//       <View>
+//         <Text>DoctorChat</Text>
+//       </View>
+//     );
+//   }
+// }
+
+const DoctorChat = () => {
+ return (
+   <View>
+     <Text>DoctorChat</Text>
+   </View>
+ );
 }
+
+export default DoctorChat
 
 const styles = StyleSheet.create({});
